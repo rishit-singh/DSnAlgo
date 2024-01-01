89a9e981837390807d93b156fabdf120feb472ab
rishit-singh <rsrishitsingh@gmail.com>
2023-12-31 18:13:55 -0800

    Defined PriorityQueue<T>::GetHeapMax() & PriorityQueue<T>::ExtractHeapMax()

--- Trees/Heap/priority_queue.hpp
+++ Trees/Heap/priority_queue.hpp
@@ -3,6 +3,7 @@
 
 #include <cstdlib>
 #include <unordered_map>
+#include <stdexcept>
 #include "heap.h"
 
 template<typename T = uint32_t>
@@ -28,19 +29,49 @@ class PriorityQueue
 private:
 	friend class Heap<T>;	
 
-	Heap<T> MaxHeap;
-	
+	Heap<T> MaxHeap;	
+
+	std::unordered_map<int, size_t> IndexMap;
+
 	T GetHeapMax() const;
 
 	T ExtractHeapMax(); 
 public:
 	
-	void Insert(); 
-	void IncreaseKey(KeyValue<T>, int key);
+	void Insert(KeyValue<T>); 
+	void IncreaseKey(KeyValue<T>, int);
 
 	PriorityQueue() = default;
 	~PriorityQueue() = default;
 };
 
+template<typename T>
+T PriorityQueue<T>::GetHeapMax() const
+{
+	if (this->MaxHeap.GetSize() < 1)
+		throw std::runtime_error("Heap underflow");
+
+
+	return this->MaxHeap[0];
+}
+
+template<typename  T>
+T PriorityQueue<T>::ExtractHeapMax()
+{
+	T max = this->GetHeapMax();
+
+	this->MaxHeap.Buffer[0] = this->MaxHeap[this->MaxHeap.GetSize() - 1];
+
+	this->MaxHeap.Size--;
+
+	this->MaxHeap.GenerateMax(0);
+	
+	return max;
+}
+
+template<typename T>
+void PriorityQueue<T>::IncreaseKey(KeyValue<T> object, int key)
+{
+}
 
 #endif
